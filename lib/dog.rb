5dd006cd62e97f4eeba3b8e7e67c28ba20fe2957
Mia Pan <miapanyuewei@gmail.com>
2019-10-29 23:22:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,15 +8,7 @@ class Dog
   def breed 
     @the_dog_breed
   end
-  def initialize(name)
-    @name=name
-  end 
-  def name=(name)
-    @the_dog_name=name
-  end 
-  def name
-    @the_dog_name
-  end
+  
 end
 
 
