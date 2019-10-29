b9efb5346dff0e8121dc82b500c7216ea27dcf20
Mia Pan <miapanyuewei@gmail.com>
2019-10-29 23:32:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,22 @@
 class Dog 
-  def initialize(name,breed=Mutt)
+  def initialize(name,breed=nil)
     @name=name
+    @breed=breed
   end 
-  def name=(name, breed=Mutt)
+  def name=(name, breed=nil)
     @the_dog_name=name
+    @the_dog_breed=breed
   end 
   def name
     @the_dog_name
   end
-  
+  def breed
+    @the_dog_breed
+  end
 end
 fido=Dog.new(Mutt)
 fido.breed
+
 
 
     
